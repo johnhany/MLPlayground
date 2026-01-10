a44ba17aa77a478b12ad99138970bafb39cca863
John Hany <johnhany@163.com>
2026-01-10 23:40:32 +0800

    unsloth qwen3

---
+++ LLM/unsloth/unsloth_test.py
@@ -0,0 +1,31 @@
+import os
+
+from unsloth import FastLanguageModel
+from unsloth.chat_templates import standardize_sharegpt, train_on_responses_only
+
+import torch
+
+
+max_seq_length = 1024 # Can increase for longer reasoning traces
+lora_rank = 32 # Larger rank = smarter, but slower
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/DeepSeek-R1-0528-Qwen3-8B",
+    max_seq_length = max_seq_length,
+    load_in_4bit = True, # False for LoRA 16bit
+    fast_inference = True, # Enable vLLM fast inference
+    max_lora_rank = lora_rank,
+    gpu_memory_utilization = 0.9, # Reduce if out of memory
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = lora_rank, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = [
+        "q_proj", "k_proj", "v_proj", "o_proj",
+        "gate_proj", "up_proj", "down_proj",
+    ],
+    lora_alpha = lora_rank*2, # *2 speeds up training
+    use_gradient_checkpointing = "unsloth", # Reduces memory usage
+    random_state = 3407,
+)
